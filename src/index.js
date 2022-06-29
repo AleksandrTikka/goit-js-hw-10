0cd5c000e927371567df12c71b4224a1697daf77
AleksandrTikka <alexandertikka@gmail.com>
2022-06-29 22:39:21 +0300

    29.06 commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './css/styles.css';
 import { fetchСountries } from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
-const name = '';
+
 
 const refs = {
     input: document.querySelector('#search-box'),
@@ -61,13 +61,15 @@ function renderCountriesList({
     name,
 }) {
     return `<li class country-list__item>
-    <img class="country-list__img"
+    
+    <img class = "country-list__img"
     src="${flags.svg}" 
     alt= "${name.official}" 
-    width= "50"
+    width= "30"
     />
     <h2 class "country-list__header">${name.official}</h2>
-    </li>
+     
+    </li>     
     
     `    
 };
@@ -104,34 +106,7 @@ function renderCountryInfo({
     }
 
 
-export function countryСardTemplate({
-  
-}) {
-  return `
-    <div class="country-info__container">
-      <div class="country-info__wrapper">
-        <img class="country-info__flags" src="${flags.svg}" alt="${
-    name.official
-  }" width="100" />
-        <h2 class="country-info__name">${name.official}</h2>
-      </div>
-      <p class="country-info__capital"><span class="country-info__weight">Capital:</span> ${capital}</p>
-      <p class="country-info__population"><span class="country-info__weight">Population:</span> ${population}</p>
-      <p class="country-info__languages"><span class="country-info__weight">Languages:</span> ${Object.values(
-        languages
-      )}</p>
-    </div>
-  `;
-}
 
-export function countryListTemplate({ flags, name }) {
-  return `
-  <li class="country-list__item">
-    <img class="country-list__flags" src="${flags.svg}" alt="${name.official}" width="30" />
-    <h2 class="country-list__name">${name.official}</h2>
-  </li>
-  `;
-}
 
 
 
